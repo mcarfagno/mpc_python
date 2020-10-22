385daab272dfb5c53747e3f6d15c15f18d6e0626
mcarfagno <m.carfagno@outlook.com>
2020-10-22 21:43:47 +0100

    updated location of old notebooks

--- .old/cte/mpc_demo/mpc_config.py
+++
@@ -1,6 +0,0 @@
-class Params():
-    def __init__(self):
-        self.N = 3 #number of state variables
-        self.M = 2 #number of control variables
-        self.T = 20 #Prediction Horizon
-        self.dt = 0.25 #discretization step
